f93beb4f7494bd1964063bf71f10c2ae176f1631
bjh7013 <60734701+bjh7013@users.noreply.github.com>
2021-07-30 17:20:04 +0900

    Create Mygumi_정수삼각형.java

---
+++ 알고리즘/정수 삼각형/Mygumi_정수삼각형.java
@@ -0,0 +1,26 @@
+/*
+  전형적인 점화식 이용 DP
+*/
+import java.util.*;
+class Solution {
+    public int solution(int[][] triangle) {
+        int answer = 0;
+        for(int i=1;i<triangle.length;i++){
+            for(int j=0;j<triangle[i].length;j++){
+                if(j==0){
+                    triangle[i][j] += triangle[i-1][j];
+                }else if(j==triangle[i].length - 1){
+                    triangle[i][j] += triangle[i-1][j-1];
+                }else{
+                    triangle[i][j] = Math.max(
+                        triangle[i][j] + triangle[i-1][j-1],
+                        triangle[i][j] + triangle[i-1][j]);
+                }
+            }   
+        }
+        for(int i=0;i<triangle[triangle.length-1].length;i++){
+            answer = Math.max(answer,triangle[triangle.length-1][i]);
+        }
+        return answer;
+    }
+}

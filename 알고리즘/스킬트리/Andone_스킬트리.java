8e3dabe07f2110bd8bbcb31f589b421d03fc7868
AndoneKwon <45382126+AndoneKwon@users.noreply.github.com>
2021-08-02 05:18:53 +0900

    Create Andone_스킬트리.java

---
+++ 알고리즘/스킬트리/Andone_스킬트리.java
@@ -0,0 +1,44 @@
+class Node {
+    char skillName;
+    Node nextNode;
+    
+    Node (char name, Node nextNode) {
+        this.skillName = name;
+        this.nextNode = nextNode;
+    }
+}
+
+class Solution {
+    public int solution(String skill, String[] skill_trees) {
+        int answer = 0;
+        
+        Node rootNode = new Node(skill.charAt(0), null);
+        Node head = rootNode;
+        
+        for(int i=1;i<skill.length();i++) {
+            rootNode.nextNode = new Node(skill.charAt(i), null);
+            rootNode = rootNode.nextNode;
+        }
+        
+        for(int i=0;i<skill_trees.length;i++) {
+            Node now = head;
+            boolean flag = true;
+            String treeNow = skill_trees[i];
+            for(int j=0;j<treeNow.length();j++) {
+                char charNow = treeNow.charAt(j);
+                if(!skill.contains(String.valueOf(charNow))) {
+                    continue;
+                } else if(skill.contains(String.valueOf(charNow)) && now.skillName == charNow) {
+                    now = now.nextNode;
+                    continue;
+                } else {
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag) answer++;
+        }
+        
+        return answer;
+    }
+}

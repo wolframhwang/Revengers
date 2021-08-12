76578bb96e04f4619eebbfd0eae64d7c338a0f76
InTheSecondHalf <33090896+sangdo913@users.noreply.github.com>
2021-08-13 02:31:41 +0900

    Create sangdo_뉴스 클러스터링.py

---
+++ 알고리즘/뉴스 클러스터링/sangdo_뉴스 클러스터링.py
@@ -0,0 +1,36 @@
+'''
+파이썬 아니면 개극혐일듯!
+'''
+
+from collections import defaultdict
+import re
+def solution(str1, str2):
+    str1 = str1.lower()
+    str2 = str2.lower()
+    dic1 = defaultdict(int)
+    dic2 = defaultdict(int)
+    dic3 = defaultdict(int)
+    dic4 = defaultdict(lambda: 0x3f3f3f3f)
+    regex = re.compile('[a-z]{2}')
+    for s in [str1[i:i+2] for i in range(len(str1)-1)]:
+        res = re.match(regex, s)
+        if res:
+            dic1[s]+=1
+    
+    for s in [str2[i:i+2] for i in range(len(str2)-1)]:
+        res = re.match(regex, s)
+        if res:
+            dic2[s]+=1
+    for k in dic1.keys():
+        dic3[k] = max(dic1[k], dic2[k])
+        dic4[k] = min(dic2[k], dic1[k])
+    for k in dic2.keys():
+        dic3[k] = max(dic1[k], dic2[k])
+        dic4[k] = min(dic1[k], dic2[k])
+    sum1,sum2  = 0,0
+    
+    for k in dic3.keys():
+        sum1 += dic3[k]
+    for k in dic4.keys():
+        sum2 += dic4[k]
+    return(65536*sum2//sum1 if sum1 != 0 else 65536) 

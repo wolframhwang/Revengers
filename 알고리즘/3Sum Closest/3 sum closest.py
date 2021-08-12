bb11a804cec51c62fedf1449e462b36787f8596d
Dinoryong <biiregrey2@gmail.com>
2021-08-12 12:14:32 +0900

    Create : create 3 sum closest.py solution

---
+++ 알고리즘/3Sum Closest/3 sum closest.py
@@ -0,0 +1,30 @@
+"""
+[Topics]
+
+
+[Sketch]
+
+
+"""
+class Solution(object):
+    def threeSumClosest(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        diff = float('inf')
+        nums.sort()
+        for i in range(len(nums)):
+            lo, hi = i + 1, len(nums) - 1
+            while (lo < hi):
+                sum = nums[i] + nums[lo] + nums[hi]
+                if abs(target - sum) < abs(diff):
+                    diff = target - sum
+                if sum < target:
+                    lo += 1
+                else:
+                    hi -= 1
+            if diff == 0:
+                break
+        return target - diff        

de4ab82b74bddd31d6c66b7a78fee1eca4c2e7f8
InTheSecondHalf <33090896+sangdo913@users.noreply.github.com>
2021-08-14 03:28:24 +0900

    Update sagndo_방금그곡.py

--- 알고리즘/방금그곡/sagndo_방금그곡.py
+++ 알고리즘/방금그곡/sagndo_방금그곡.py
@@ -1,58 +1,39 @@
 ```
 코틀린 없는 망문제
-만약 파이썬 아니면 찾을때 kmp 구현해야 할듯.
+만약 정규식이나 stl 아니면 문자열 비교 할 때 kmp 구현해야 할듯.
 ```
-
 import re
-class INFO:
-    def __init__(self, size, st, content, title):
-       	self.size = size
-        self.st = st
-        self.content = content
-        self.title = title
-    def __lt__(self, other):
-        if self.size != other.size: return self.size > other.size
-        return self.st < other.st
-    
+
 def solution(m, musicinfos):
-    answer = ''
     li = ['C','C#','D', 'D#', 'E','F','F#', 'G', 'G#', 'A', 'E#', 'A#', 'B'] 
     alpha = 'abcdefghijklmnopqrstuvwxyz'
     dic = {li[i]: alpha[i] for i in range(len(li))}
-    i = 0
-    conv = []
     
-    while i != len(m) :
-        if i + 2 <= len(m) and m[i:i+2] in dic:
-            conv.append(dic[m[i:i+2]])
-            i+=2
-        else:
-            conv.append(dic[m[i:i+1]])
-            i+=1
-    regex = re.compile(''.join(conv))
+    def matching(m, size=-1):
+        i = 0
+        conv = []
+        while len(conv) != size:
+            if size == -1 and i == len(m): break
+            idx = i%len(m)
+            if idx + 2 <= len(m) and m[idx:idx+2] in dic:
+                conv.append(dic[m[idx:idx+2]])
+                i+=2
+            else:
+                conv.append(dic[m[idx:idx+1]])
+                i+=1
+        return ''.join(conv)
     
-    res = []
+    mm = matching(m)
+    regex = re.compile(mm)
+    res = (-1, 10000, "(None)")
     
     for ii, m in enumerate(musicinfos):
         info = m.split(',')
         st = int(info[0][0:2])*60 + int(info[0][3:])
         ed = int(info[1][0:2])*60 + int(info[1][3:])
         size = ed-st
-        li = []
-        i = 0
-        
-        while len(li) != size:
-            idx = i%len(info[3])
-            if idx + 2 <= len(info[3]) and info[3][idx:idx+2] in dic:
-                li.append(dic[info[3][idx:idx+2]])
-                i += 2
-            else:
-                li.append(dic[info[3][idx:idx+1]])
-                i+=1
-        s = ''.join(li)
-        if regex.search(s):
-            res.append(INFO(size, ii, s, info[2]))
-    
-    if not res: return "(None)"
-    res.sort()
-    return res[0].title
+        s = matching(info[3], size)
+        data = (size, -ii, info[2])
+        if regex.search(s) and res < data: res = data
+            
+    return res[2]
